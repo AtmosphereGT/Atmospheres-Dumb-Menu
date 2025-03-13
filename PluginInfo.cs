486efadc1ae1845a22d24cc82f8f4080a5f808ca
AtmosphereGT <ozzydarby15@icloud.com>
2025-03-13 18:44:22 +0000

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.atmospheregt.gorillatag.menutemplate";
+        public const string Name = "Atmospheres's Dumb Menu";
+        public const string Description = "Created by AtmosphereGT with love <3";
+        public const string Version = "1.0.1";
+    }
+}
